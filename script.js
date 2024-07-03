3120ced6b0110875c84e24a9080731db75ad39d2
Yash Rajput <to.yashrajput@gmail.com>
2024-07-03 09:25:19 -0400

    Calculator App | Basic functions completed, just simple fixes now

---
+++ script.js
@@ -0,0 +1,68 @@
+let cInput = "";
+let pInput = "";
+let operator = "";
+
+function number(num) {
+  cInput = "";
+  cInput += num;
+  document.getElementById("display").value = cInput;
+  console.log(cInput);
+}
+
+function add() {
+  pInput = cInput;
+  operator = "+";
+  cInput = "";
+  console.log(cInput, operator);
+}
+
+function subtract() {
+  pInput = cInput;
+  operator = "-";
+  cInput = "";
+  console.log(cInput, operator);
+}
+
+function multiply() {
+  pInput = cInput;
+  operator = "*";
+  cInput = "";
+  console.log(cInput, operator);
+}
+
+function divide() {
+  pInput = cInput;
+  operator = "/";
+  cInput = "";
+  console.log(cInput, operator);
+}
+
+function submit() {
+  let result = 0;
+  let prev = parseFloat(pInput);
+  let current = parseFloat(cInput);
+
+  if (isNaN(prev) || isNaN(current)) return;
+
+  switch (operator) {
+    case "+":
+      result = prev + current;
+      break;
+    case "-":
+      result = prev - current;
+      break;
+    case "*":
+      result = prev * current;
+      break;
+    case "/":
+      result = prev / current;
+      break;
+    default:
+      return;
+  }
+
+  console.log(prev + " | " + current + " | " + result + " | " + operator);
+  currentInput = result;
+  operator = "";
+  document.getElementById("display").value = result;
+}
