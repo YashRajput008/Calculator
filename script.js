f3ae8f577a976610f6e4c89aa6ae58a47b149ca4
Yash Rajput <to.yashrajput@gmail.com>
2024-07-03 19:24:49 -0400

    Almost finished with project, added 8 more features + cleaned up

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ let cInput = "";
 let pInput = "";
 let operator = "";
 
+function updateDisplay() {
+  document.getElementById("display").value = cInput || pInput;
+}
+
 function number(num) {
-  cInput = "";
   cInput += num;
-  document.getElementById("display").value = cInput;
+  updateDisplay();
   console.log(cInput);
 }
 
@@ -39,6 +42,13 @@ function divide() {
   console.log(cInput, operator);
 }
 
+function power() {
+  pInput = cInput;
+  operator = "^";
+  cInput = "";
+  console.log(cInput, operator);
+}
+
 function submit() {
   let result = 0;
   let prev = parseFloat(pInput);
@@ -59,6 +69,9 @@ function submit() {
     case "/":
       result = prev / current;
       break;
+    case "^":
+      result = Math.pow(prev, current);
+      break;
     default:
       return;
   }
@@ -76,7 +89,50 @@ function sqrt() {
   document.getElementById("display").value = cInput;
 }
 
-function cube() {
+function cbrt() {
   cInput = Math.cbrt(parseFloat(cInput));
   document.getElementById("display").value = cInput;
+}
+
+function square() {
+  cInput = Math.pow(parseFloat(cInput), 2);
+  document.getElementById("display").value = cInput;
+}
+
+function cube() {
+  cInput = Math.pow(parseFloat(cInput), 3);
+  document.getElementById("display").value = cInput;
+}
+
+// Clear Function (difference b/w clear() and clearDisplay())
+
+function clearDisplay() {
+  cInput = "";
+  pInput = "";
+  operator = "";
+  document.getElementById("display").value = "";
+
+  console.log("clear");
+}
+
+function decimal() {
+  if (cInput.indexOf(".") == -1) {
+    cInput += ".";
+    document.getElementById("display").value = cInput;
+    console.log("Added Decimal Point");
+  } else {
+    console.log("Can't add another Decimal Point");
+  }
+}
+
+function backspace() {
+  cInput = cInput.substring(0, cInput.length - 1);
+  document.getElementById("display").value = cInput;
+}
+
+function symbol() {
+  if (cInput !== "") {
+    cInput = parseFloat(cInput) * -1;
+    document.getElementById("display").value = cInput;
+  }
 }
